1afe0e2c0493afc7b207e7071a1619b8beb040eb
unknown <shadab105662@gmil.com>
2022-05-18 12:10:05 +0530

    with test cases

--- demo-app/src/main/java/com/shadab/App.java
+++ demo-app/src/main/java/com/shadab/App.java
@@ -17,22 +17,36 @@ public class App {
 		printFebonacci(N);
 	}
 
-	private static void printFebonacci(int N) {
+	public static String printFebonacci(int N) {
 
 		int a = 0;
 		int b = 1;
-
-		System.out.println(a);
-		System.out.println(b);
-
-		for (int i = 3; i <= N; i++) {
-			int c = a + b;
-
-			System.out.println(c);
-
-			a = b;
-			b = c;
+		
+		if(N <=0) {
+			
+			return "Invalid input";
 		}
 
+		String series = "";
+
+		if (N == 1) {
+			series = "0";
+
+		} else if (N == 2) {
+			series = "0 1";
+		} else {
+			for (int i = 3; i <= N; i++) {
+				int c = a + b;
+
+				series = series + " " + c;
+
+				a = b;
+				b = c;
+			}
+
+			System.out.println(series);
+
+		}
+		return series;
 	}
 }
--- demo-app/src/test/java/com/shadab/AppTest.java
+++ demo-app/src/test/java/com/shadab/AppTest.java
@@ -1,8 +1,54 @@
 package com.shadab;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+	@Test
+	public void test_printFebonaci() {
+
+		String expected = "0 1 1 2 3 5 8 13 21 34";
+		int N = 10;
+
+		String actual = App.printFebonacci(N);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test_printFebonaci_WithSizeOne() {
+
+		String expected = "0";
+		int N = 1;
+
+		String actual = App.printFebonacci(N);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test_printFebonaci_WithSizeTwo() {
+
+		String expected = "0 1";
+		int N = 2;
+
+		String actual = App.printFebonacci(N);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test_printFebonaci_WithSizeZero() {
+
+		String expected = "Invalid input";
+		int N = 0;
+
+		String actual = App.printFebonacci(N);
+
+		assertEquals(expected, actual);
+	}
 }
