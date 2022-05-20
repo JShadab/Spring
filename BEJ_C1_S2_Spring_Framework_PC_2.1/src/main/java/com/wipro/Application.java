221971e5b8735395465f0b759ba5ac16169370fa
unknown <shadab105662@gmil.com>
2022-05-20 11:49:51 +0530

    Assignemnt-1

---
+++ BEJ_C1_S2_Spring_Framework_PC_2.1/src/main/java/com/wipro/Application.java
@@ -0,0 +1,18 @@
+package com.wipro;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+		Product product = context.getBean(Product.class);
+
+		System.out.println(product);
+
+	}
+
+}
---
+++ BEJ_C1_S2_Spring_Framework_PC_2.1/src/main/java/com/wipro/Product.java
@@ -0,0 +1,38 @@
+package com.wipro;
+
+public class Product {
+
+	private String name;
+	private String category;
+	private double price;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "[name=" + name + ", category=" + category + ", price=" + price + "]";
+	}
+
+}
