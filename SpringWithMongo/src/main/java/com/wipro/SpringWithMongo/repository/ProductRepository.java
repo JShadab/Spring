63490c88ea875a6df316d1b8723555a3d60aeefb
unknown <shadab105662@gmil.com>
2022-06-07 10:23:38 +0530

    delete Spring Mockito

--- SpringWithMongo/src/main/java/com/wipro/SpringWithMongo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.SpringWithMongo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.wipro.SpringWithMongo.entity.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
