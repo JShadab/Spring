9549774874b3fbc3f469cd17bff4bab63b6e30dc
unknown <shadab105662@gmil.com>
2022-06-09 11:32:06 +0530

    Remove Spring Securit with JWT-1

---
+++ SecurityDemo/src/main/java/com/wipro/niit/SecurityDemo/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.wipro.niit.SecurityDemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+//		auth.inMemoryAuthentication().withUser("Jayasree").password("1234").roles("NORM_USER").and().withUser("Raj")
+//				.password("4321").roles("ADMIN");
+
+		auth.userDetailsService(userDetailsService);
+
+	}
+
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+}
